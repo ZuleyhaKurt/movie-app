9d32b4534987d36b1458a09192ee0de1fa0fcb82
Züleyha Kurt <netice2017@gmail.com>
2022-11-12 03:03:16 +0300

    detail

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,73 @@
-import React from 'react'
+import axios from 'axios'
+import React, {  useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Card = ({ item, index }) => {
+
+const Card = ({ films, setFilms }) => {
+    const [query,setQuery] = useState("")
+    const [loading,setLoading] = useState(false)
+    const navigate= useNavigate()
     
-    return (
-    <div className=" relative overflow-hidden card w-[25rem] h-[25rem] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800          dark:border-gray-700" key={index}>
-       
-        <img className='w-[25rem] h-[22rem]' src={`https://image.tmdb.org/t/p/w1280${item?.poster_path}`} alt="" />
-               
-        <div className='bg-slate-400 h-[3rem] flex justify-between px-4 items-center'>
-        <a href="">{item?.title} </a>
-                <span className='px-2 h-[2rem] rounded-lg bg-yellow-400'>{item?.vote_average}</span>
-            </div>
-           
-        <p className="font-normal text-black overview  bg-slate-400 opacity-80 absolute bottom-[3rem]">{item?.overview}</p>
+    const getMovie = async () => {
+        setLoading(true)
+        try {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=2a72939572bf0f6322fd87c528cc4cdd&query=${query}`
+        const movDetail = await axios(url)
+        const { data: { results } } = movDetail
+        setFilms(results)
+        setLoading(false)
+        } catch (error) {
+            console.log(error)
+        }
         
-    </div>
+    }
 
+    const handleSearch = () => {
+        getMovie()
+        setQuery("")
+  }
+   
+
+    
+ 
+
+    return (
+        <>
+            <div className="flex my-5 gap-5  justify-center">
+      <input type="text" name="" id="" value={query} onChange={(e)=>setQuery(e.target.value)} className='w-[15rem] rounded-lg indent-3 border-solid border-inhert border-1 outline outline-inherit' />
+                  
+      <button   className='bg-red-600 py-1 px-5 font-bold text-md text-slate-300 rounded-md active:scale-95 active:bg-slate-300 active:text-slate-600 duration 'onClick={handleSearch}>Search</button>
+            </div>
+            <div className='flex flex-wrap gap-10 justify-center'>
+            {loading ? (<h1>Loading</h1>)
+                :
+                ( films?.map((item,index) => {
+          
+                    return ( 
+                        <div className=" relative overflow-hidden card w-[25rem] h-[25rem] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800          dark:border-gray-700" key={index}>
+       
+                            <img className='w-[25rem] h-[22rem]'
+                                // onClick={() => navigate(`/movieDetail`, { state: item })}
+                                src={`https://image.tmdb.org/t/p/w1280${item?.poster_path}`} alt="" />
+                      
+                            <div className='bg-slate-400 h-[3rem] flex justify-between px-4 items-center'>
+                           <a href="">{item?.title} </a>
+                            <span className='px-2 h-[2rem] rounded-lg bg-yellow-400'>{item?.vote_average}</span>
+                            </div>
+                  
+                             <p className="font-normal text-black overview  bg-slate-400 opacity-80 absolute bottom-[3rem]">{item?.overview}</p>
+               
+                            </div>
+                    )
+                 }) )
+                }
+
+            </div>
+                  
+            
+            
+    
+    </>
   )
 }
 
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const MovieDetail = () => {
+  const { state: item } = useLocation()
+  console.log(item.id)
+  const navigate = useNavigate()
+  const [details, setDetails] = useState("")
+  const [videoDetail,setVideoDetail]= useState("")
+  const [random,setRandom]= useState("")
+  
+ 
+  const API_KEY = "2a72939572bf0f6322fd87c528cc4cdd"
+  const ID=item.id
+ 
+  const getDetail = async () => {
+    const url = `https://api.themoviedb.org/3/movie/${ID}?api_key=${API_KEY}`
+    const detail = await axios(url)
+    setDetails(detail.data)
+  }
+
+  const getVideo = async () => {
+    const url = `https://api.themoviedb.org/3/movie/${ID}/videos?api_key=${API_KEY}`
+    const video = await axios(url)
+    const{data}=video
+    setVideoDetail(data.results)
+    setRandom(Math.floor((Math.random() * (data.results.length))))
+   }
+// useEffect(() => {
+//   getDetail()
+//   getVideo()
+// }, [])
+
   return (
-    <div>MovieDetail</div>
+    <>
+    <h1 className='text-gray-900 text-4xl font-bold text-center'>{details?.original_title}</h1>
+    <div className="flex justify-center w-10/12 md:w-3/5 mx-auto my-3" >
+        {/* <iframe src={`https://www.youtube.com/embed/${videoDetail[random]?.key}?autoplay=1&mute=1`} frameborder="0" allowfullscreen></iframe> */}
+    </div>
+    <div className="flex justify-center">
+        <div className="flex flex-col lg:flex-row max-w-6xl  rounded-lg bg-white shadow-lg">
+            <img className="lg:w-1/3 h-96 lg:h-[600px] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg" src={`https://image.tmdb.org/t/p/w1280${details?.poster_path}`} alt="" />
+            <div className="flex flex-col justify-start h-[28rem]">
+                <h5 className="text-gray-900 text-xl font-medium border-2 h-12">Overview</h5>
+                <p className="text-gray-700 text-base border-2 h-80">
+                    {details?.overview}
+                </p>
+                <p className="text-gray-600 text-xs border-2">Release Date: {details?.release_date}</p>
+                <p className="text-gray-600 text-xs border-2">Rate: {details?.vote_average?.toFixed(1)}</p>
+                <p className="text-gray-600 text-xs border-2">Total Vote: {details?.vote_count}</p>
+                {/* <p className="text-gray-600 text-xs border-2"> <button onClick={()=>navigate(-1)}>Go Back</button></p> */}
+            </div>
+        </div>
+    </div>
+</>
   )
 }
 
